77d9023acae77166e6f836dd1a882430bc708d7d
Xavier Blijd <21082510@student.hhs.nl>
2025-06-25 14:48:44 +0200

    Camera curve en split screen mobile and desktop version

--- src/Experience/Experience.jsx
+++ src/Experience/Experience.jsx
@@ -1,6 +1,8 @@
 import { React, Suspense, useRef} from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
-import { CameraControls, Environment, View } from '@react-three/drei'
+import { CameraControls, OrbitControls, Environment, PerspectiveCamera, View, useHelper } from '@react-three/drei'
+
+import * as THREE from "three"
 
 import House from './components/HouseTransformed'
 import OakTrees from './components/OakTreesTransformed'
@@ -17,35 +19,32 @@ import GroundMiddle from './components/GroundMiddleTransformed'
 import GroundSouth from './components/GroundSouthTransformed'
 import Museum from './components/MuseumGebouwTransformed'
 
-const Scene = ({controls}) => {
-    useFrame(() => {
-        // console.log("position")
-        // console.log(controls.current.getPosition())
-        // console.log("rotation")
-        // console.log(controls.current.camera.rotation())
-    })
+const CameraHelper = ({cameraRef}) => {
+  useHelper(cameraRef, THREE.CameraHelper)
+
+  return null;
 }
 
-const Experience = () => {
-    const controls = useRef()
+const DebugCurve = ({curve})
 
-  return (
-    <>
-      <Canvas 
-        camera= {{
-          position: [-64, 34, -26],
-          fov: 90,
-          }}>  
+const Scene = ({camera}) => {
 
-        <CameraControls ref = {controls}/>
+  const cameraCurve = new THREE.CatmullRomCurve3 ([
+    new THREE.Vector3(-65, 33, 29),
+    new THREE.Vector3(-65, 33, -14),
+    new THREE.Vector3(-60, 33, -2)
+  ])
 
-
-        {/* <mesh>
-            <boxGeometry/>
-            <meshStandardMaterial/>
-        </mesh>
-        <directionalLight position={[0, 0, 5]} color ="red"/> */}
-        <Environment 
+    useFrame(() => {
+      if(camera){
+        console.log("position")
+        console.log(camera.current.position)
+        console.log("rotation")
+        console.log(camera.current.rotation)
+      }
+    })
+return (<>
+     <Environment 
         background={true}
         files={[
           '/cubemap/px.webp', 
@@ -57,9 +56,8 @@ const Experience = () => {
           ]} 
         />
 
-        <Scene controls={controls} />
-        <ambientLight intensity={5} />
-        <Suspense fallback={null}>
+
+      <Suspense fallback={null}>
         <Azalea/>
         <Campfire/>
         <Extra/>
@@ -74,7 +72,55 @@ const Experience = () => {
         <OakTrees/>
         <CherryTrees/>
         <Bridge/>
-        </Suspense>
+      </Suspense>
+    </>
+  )
+}
+
+const Experience = () => {
+    const controls1 = useRef()
+    const controls2 = useRef()
+    const camera1 = useRef()
+    const camera2 = useRef()
+
+  return (
+    <>
+
+      <View 
+        style ={{ 
+          width: 402, 
+          height: 875, 
+          position: "absolute", 
+          top: 0, 
+          right: 0
+        }}
+        >
+        <Scene camera={camera1}/>
+        <PerspectiveCamera ref={camera1} makeDefault fov={90} position={[-64, 34, -26]} />
+        <OrbitControls ref={controls1} camera={camera1.current}/>
+        </View>, position: "absolute", top: 0
+      <View 
+        style ={{ 
+          width: 750, 
+          height: 750, 
+          position: "absolute", 
+          top: 0
+          }}
+        >
+        <PerspectiveCamera ref={camera2} makeDefault fov={90} position={[-64, 34, -26]} />
+        <Scene/>
+        <CameraControls ref={controls2} camera={camera2.current}/>'
+        <CameraHelper cameraRef={camera1}/>
+      </View>
+      <Canvas
+      eventSource={document.getElementById("root")}
+        // camera= {{
+        //   position: [-64, 34, -26],
+        //   fov: 90,
+        //   }}
+      >  
+
+        <View.Port/>
       </Canvas>
     </>
   )
